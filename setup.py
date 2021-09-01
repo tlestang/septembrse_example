a163517ae3a50e70b362e8f90b31cbccd24a974b
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-09-01 16:28:46 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""Setup example package
+"""
+from setuptools import find_packages
+from skbuild import setup
+
+setup(
+    name='septembrse',
+    license='GPLv3',
+    description='A simple example package for the walkthrough',
+    author='Thibault Lestang',
+    author_email='thibault.lestang@protonmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    cmake_install_dir="src/example_pkg",
+)
